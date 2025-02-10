0058b1b05999c80a55fe3f72d0f8c5273e244584
AkashdeepArjun <wadhwaarjun007@gmail.com>
2025-02-10 21:08:49 +0530

    added new theme

--- init.lua
+++ init.lua
@@ -4,26 +4,56 @@ require 'core.keymaps'
 
 local lazypath = vim.fn.stdpath 'data' .. '/lazy/lazy.nvim'
 if not (vim.uv or vim.loop).fs_stat(lazypath) then
-    local lazyrepo = 'https://github.com/folke/lazy.nvim.git'
-    local out = vim.fn.system { 'git', 'clone', '--filter=blob:none', '--branch=stable', lazyrepo, lazypath }
-    if vim.v.shell_error ~= 0 then
-        error('Error cloning lazy.nvim:\n' .. out)
-    end
+  local lazyrepo = 'https://github.com/folke/lazy.nvim.git'
+  local out = vim.fn.system { 'git', 'clone', '--filter=blob:none', '--branch=stable', lazyrepo, lazypath }
+  if vim.v.shell_error ~= 0 then
+    error('Error cloning lazy.nvim:\n' .. out)
+  end
 end ---@diagnostic disable-next-line: undefined-field
 vim.opt.rtp:prepend(lazypath)
 
-require('lazy').setup {
+local default_color_scheme = 'nord'
+local env_var_nvim_theme = os.getenv 'NVIM_THEME' or default_color_scheme
 
-    require 'plugins.colortheme',
-    require 'plugins.neo-tree',
-    require 'plugins.lualine',
-    require 'plugins.bufferline',
-    require 'plugins.telescope',
-    require 'plugins.lsp',
-    require 'plugins.autocomplete',
-    require 'plugins.none-ls',
-    require 'plugins.gitsigns',
-    require 'plugins.alpha',
-    require 'plugins.indent-blankline',
-    require 'plugins.misc',
+local themes = {
+  nord = 'plugins.themes.nord',
+  onedark = 'plugins.themes.onedark',
 }
+
+require('lazy').setup({
+  -- require(themes[env_var_nvim_theme]),
+  -- require 'plugins.colortheme',
+  require 'plugins.themes.kanagawa',
+  require 'plugins.neo-tree',
+  require 'plugins.lualine',
+  require 'plugins.bufferline',
+  require 'plugins.telescope',
+  require 'plugins.lsp',
+  require 'plugins.autocomplete',
+  require 'plugins.none-ls',
+  require 'plugins.gitsigns',
+  require 'plugins.alpha',
+  require 'plugins.indent-blankline',
+  require 'plugins.misc',
+}, {
+
+  ui = {
+    -- If you have a Nerd Font, set icons to an empty table which will use the
+    -- default lazy.nvim defined Nerd Font icons otherwise define a unicode icons table
+    icons = vim.g.have_nerd_font and {} or {
+      cmd = '⌘',
+      config = '🛠',
+      event = '📅',
+      ft = '📂',
+      init = '⚙',
+      keys = '🗝',
+      plugin = '🔌',
+      runtime = '💻',
+      require = '🌙',
+      source = '📄',
+      start = '🚀',
+      task = '📌',
+      lazy = '💤 ',
+    },
+  },
+})
--- lua/plugins/themes/nord.lua
+++ lua/plugins/themes/nord.lua
@@ -1,6 +1,6 @@
 return {
   'shaunsingh/nord.nvim',
-  lazy = true,
+  lazy = false,
   priority = 1000,
   config = function()
     vim.g.nord_contrast = true
